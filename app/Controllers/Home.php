75627bcc0d942e105b273a7bbd6c804e8cf00502
dtnngts <dota.ningtias100219@students.unila.ac.id>
2021-09-16 22:29:09 +0700

    dtnngts

---
+++ app/Controllers/Home.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	public function fungsiBaru($nama, $npm) {
+		echo "Hello $nama";
+		echo "<br> $npm";
+	}
+
+	public function show() {
+		$adat['nama'] = 'Dota Ningtias';
+		$adat['jurusan'] = 'Ilmu Komputer';
+		echo view("mahasiswa/index", $adat);
+	}
+}
+	}
+}
